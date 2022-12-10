b6b913ffaa73bc856a782389bb5bdf9b65d9ed9c
bczhc <bczhc0@126.com>
2022-12-10 21:11:11 +0800

    finish
    - https://stackoverflow.com/questions/58478042/cannot-run-program-data-data-my-app-package-files-my-executable-error-13-pe

--- app/src/main/java/pers/zhc/frpss/Configs.kt
+++ app/src/main/java/pers/zhc/frpss/Configs.kt
@@ -6,4 +6,18 @@ object Configs {
     var SS_PASSWORD = "password"
     var FRP_SERVER_ADDRESS = "x.x.x.x"
     var FRP_SERVER_PORT = 6666.toUShort()
+    var FRP_REMOTE_PORT = 6000.toUShort()
+
+    fun generateFrpConfig(): String {
+        return """[common]
+server_addr = $FRP_SERVER_ADDRESS
+server_port = $FRP_SERVER_PORT
+
+[frp-ss]
+type = tcp
+local_ip = 127.0.0.1
+local_port = $SS_PORT
+remote_port = $FRP_REMOTE_PORT
+"""
+    }
 }
--- app/src/main/java/pers/zhc/frpss/MainActivity.kt
+++ app/src/main/java/pers/zhc/frpss/MainActivity.kt
@@ -1,8 +1,10 @@
 package pers.zhc.frpss
 
 import android.os.Bundle
+import android.text.InputType
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
 import androidx.annotation.RawRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,8 @@ import pers.zhc.frpss.databinding.ActivityMainBinding
 import pers.zhc.frpss.databinding.SettingsMenuBinding
 import pers.zhc.util.IOUtils
 import java.io.File
+import java.io.InputStream
+import java.util.*
 import java.util.zip.GZIPInputStream
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     }
     private val frpcBin by lazy { File(binDir, "frpc") }
     private val ssserverBin by lazy { File(binDir, "ssserver") }
+    private val frpConfigFile by lazy { File(filesDir, "frpc.ini") }
     private val dialogs = object {
         val settings by lazy { createSettingsDialog() }
     }
@@ -41,15 +46,63 @@ class MainActivity : AppCompatActivity() {
         val ssButton = bindings.ssStartBtn
 
         frpButton.setOnClickListener {
-
+            runOnUiThread { frpButton.isEnabled = false }
+            executeFrp(frpLogET)
+        }
+        ssButton.setOnClickListener {
+            runOnUiThread { ssButton.isEnabled = false }
+            executeSS(ssLogET)
         }
     }
 
-//    private fun execute(binFile: File, logET: EditText) {
-//        Thread {
-//            Runtime.getRuntime().exec()
-//        }.start()
-//    }
+    private fun executeProcessOutputToET(cmd: Array<String>, et: EditText) {
+        val process = Runtime.getRuntime().exec(cmd)
+        val inputStream = process.inputStream
+        val errorStream = process.errorStream
+
+        val writeLinesThread = { stream: InputStream ->
+            Thread {
+                val scanner = Scanner(stream, "UTF-8")
+                while (scanner.hasNextLine()) {
+                    val line = scanner.nextLine()
+                    runOnUiThread {
+                        et.append(line)
+                        et.append("\n")
+                    }
+                }
+            }.start()
+        }
+        writeLinesThread(inputStream)
+        writeLinesThread(errorStream)
+
+        process.waitFor()
+    }
+
+    private fun executeFrp(logET: EditText) {
+        Thread {
+            frpConfigFile.writeText(Configs.generateFrpConfig())
+
+            executeProcessOutputToET(
+                arrayOf(
+                    frpcBin.path,
+                    "-c", frpConfigFile.path
+                ), logET
+            )
+        }.start()
+    }
+
+    private fun executeSS(logET: EditText) {
+        Thread {
+            executeProcessOutputToET(
+                arrayOf(
+                    ssserverBin.path,
+                    "--server-addr", "0.0.0.0:${Configs.SS_PORT}",
+                    "--encrypt-method", Configs.SS_ENCRYPTION,
+                    "--password", Configs.SS_PASSWORD
+                ), logET
+            )
+        }.start()
+    }
 
     private fun extractBinFiles() {
         decompressRawFile(R.raw.frpc_gzip, frpcBin)
@@ -95,17 +148,24 @@ class MainActivity : AppCompatActivity() {
         val ssPasswordET = bindings.ssPasswordEt.also { it.setText(Configs.SS_PASSWORD) }
         val frpServerPortET = bindings.frpServerPortEt.also { it.setText(Configs.FRP_SERVER_PORT.toString()) }
         val frpServerAddrET = bindings.frpServerAddressEt.also { it.setText(Configs.FRP_SERVER_ADDRESS) }
+        val frpRemotePortET = bindings.frpRemotePortEt.also { it.setText(Configs.FRP_REMOTE_PORT.toString()) }
+
+        val saveConfigs = {
+            Configs.SS_PORT = ssPortET.text.toString().toUShort()
+            Configs.SS_ENCRYPTION = ssEncryptionET.text.toString()
+            Configs.SS_PASSWORD = ssPasswordET.text.toString()
+            Configs.FRP_SERVER_ADDRESS = frpServerAddrET.text.toString()
+            Configs.FRP_SERVER_PORT = frpServerPortET.text.toString().toUShort()
+            Configs.FRP_REMOTE_PORT = frpRemotePortET.text.toString().toUShort()
+        }
 
         return MaterialAlertDialogBuilder(this)
             .setView(bindings.root)
             .setPositiveButton(android.R.string.ok) { _, _ ->
-                Configs.SS_PORT = ssPortET.text.toString().toUShort()
-                Configs.SS_ENCRYPTION = ssEncryptionET.text.toString()
-                Configs.SS_PASSWORD = ssPasswordET.text.toString()
-                Configs.FRP_SERVER_ADDRESS = frpServerAddrET.text.toString()
-                Configs.FRP_SERVER_PORT = frpServerPortET.text.toString().toUShort()
+                saveConfigs()
             }
             .setNegativeButton(android.R.string.cancel, null)
+            .setOnDismissListener { saveConfigs() }
             .create()
     }
 }
